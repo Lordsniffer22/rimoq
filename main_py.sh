cd38fb3ef56c170c89320c3305e720f804b33c78
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-06-18 14:21:05 +0300

    Update main_py.sh

--- main_py.sh
+++ main_py.sh
@@ -1,5 +1,7 @@
 docker exec -it gosty rm fia.py
-wget -O fia.py https://raw.githubusercontent.com/Lordsniffer22/rimoq/main/creds.py
+wget -O fia.py https://raw.githubusercontent.com/Lordsniffer22/rimoq/main/fia.py
+wget -O keyboards.py https://raw.githubusercontent.com/Lordsniffer22/rimoq/main/keyboards.py
 docker cp fia.py gosty:/app
+docker cp keyboards.py gosty:/app
 docker restart gosty
-rm -rf fia.py
+rm -rf fia.py keyboards.py
