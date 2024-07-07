82bb7a4d7810586a9ddf3af95f7fb33efa9b8f54
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-07-07 20:35:45 +0300

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 # Server credentials
 SERVER_CREDENTIALS = {
-    'ZA': {'host': '51.75.76.115', 'username': 'ubuntu', 'password': 'hY5va8twDndA'},
-    'BRAZIL': {'host': '31.172.83.238', 'username': 'root', 'password': 'Wu366lfIa9D57^-42Xr6P8'},
-    'USA': {'host': '172.232.29.56', 'username': 'root', 'password': '172.232.171.56'},
-    'GERMANY': {'host': '172.105.251.41', 'username': 'root', 'password': '@rootmenow1'},
-    'SG': {'host': '31.172.83.238', 'username': 'root', 'password': 'Wu366lfIa9D57^-42Xr6P8'},
-    'NL': {'host': '31.172.83.238', 'username': 'root', 'password': 'Wu366lfIa9D57^-42Xr6P8'}
+    'ZA': {'host': '102.130.125.183', 'username': 'vmrkucte', 'password': '#DjmzsBf$ozD'},
+    'BRAZIL': {'host': '31.172.83.238', 'username': 'root', 'password': 'Wu366wlfIa9D57^-42Xr6P8'},
+    'USA': {'host': '172.232.29.56', 'username': 'root', 'password': '172.2323.171.56'},
+    'GERMANY': {'host': '172.105.251.41', 'username': 'root', 'password': '@root3menow1'},
+    'SG': {'host': '31.172.83.238', 'username': 'root', 'password': 'Wu366lfIa9D57^-342Xr6P8'},
+    'NL': {'host': '31.172.83.238', 'username': 'root', 'password': 'Wu366lfIa9D57^-42Xr46P8'}
 }
