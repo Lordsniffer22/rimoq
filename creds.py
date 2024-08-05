431b430ff3318f200436d4960d09ec14f1d30aa6
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-08-05 23:22:37 +0300

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 # Server credentials
 SERVER_CREDENTIALS = {
-    'ZA': {'host': 'za1.teslassh.xyz', 'username': 'vmrkucte', 'password': '#DjmzsBf$ozD'},
-    'BRAZIL': {'host': 'br1.teslassh.xyz', 'username': 'vmrkucte', 'password': '#DjmzsBf$ozD'},
-    'USA': {'host': 'us1.teslassh.xyz', 'username': 'vmrkucte', 'password': '#DjmzsBf$ozD'},
-    'GERMANY': {'host': 'de1.teslassh.xyz', 'username': 'vmrkucte', 'password': '#DjmzsBf$ozD'},
-    'SG': {'host': 'sg1.teslassh.xyz', 'username': 'vmrkucte', 'password': '#DjmzsBf$ozD'},
-    'NL': {'host': 'nl.teslassh.xyz', 'username': 'vmrkucte', 'password': '#DjmzsBf$ozD'}
+    'ZA': {'host': 'za1.teslassh.xyz', 'username': 'vmrkucte', 'password': 'iI(.7Pg(M3J-'},
+    'BRAZIL': {'host': 'br1.teslassh.xyz', 'username': 'vmrkucte', 'password': 'iI(.7Pg(M3J-'},
+    'USA': {'host': 'us1.teslassh.xyz', 'username': 'vmrkucte', 'password': 'iI(.7Pg(M3J-'},
+    'GERMANY': {'host': 'de1.teslassh.xyz', 'username': 'vmrkucte', 'password': 'iI(.7Pg(M3J-'},
+    'SG': {'host': 'sg1.teslassh.xyz', 'username': 'vmrkucte', 'password': 'iI(.7Pg(M3J-'},
+    'NL': {'host': 'nl.teslassh.xyz', 'username': 'vmrkucte', 'password': 'iI(.7Pg(M3J-'}
 }
