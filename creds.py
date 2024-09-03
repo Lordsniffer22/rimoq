b4b9e6459aa81748d243063af162380e73a6f540
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-09-03 10:29:00 +0300

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,1 @@
-# Server credentials
-SERVER_CREDENTIALS = {
-    'ZA': {'host': 'za.teslassh.xyz', 'username': 'root', 'password': 'itmeansnothing8'},
-    'BRAZIL': {'host': 'br1.teslassh.xyz', 'username': 'root7', 'password': 'itmeansnothing8'},
-    'USA': {'host': 'us.teslassh.xyz', 'username': 'root', 'password': 'itmeansnothing8'},
-    'GERMANY': {'host': 'de8.teslassh.xyz', 'username': 'root', 'password': 'itmeansnothing8'},
-    'SG': {'host': 'sg.teslassh.xyz', 'username': 'root', 'password': 'itmeansnothing8'},
-    'NL': {'host': 'nl.teslassh.xyz', 'username': 'root', 'password': 'itmeansnothing8'}
-}
+b
